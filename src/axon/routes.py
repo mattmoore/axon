70d8361c7e4ea7b757aefdff54f8e3598fadcd75
Matt Moore <matt@mattmoore.io>
2025-02-22 22:54:01 -0500

    POST example working

--- src/axon/routes.py
+++ src/axon/routes.py
@@ -1,11 +1,28 @@
 import uvicorn
 from fastapi import FastAPI
+from pydantic import BaseModel
+from transformers import pipeline
 
 app = FastAPI()
+
+#classifier = pipeline("sentiment-analysis")
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+class SentimentAnalysisRequest(BaseModel):
+    text: str
+
+class SentimentAnalysisResponse(BaseModel):
+    analysis: str
+
+@app.post("/api/sentiment-analysis")
+async def sentiment_analysis(request: SentimentAnalysisRequest):
+    #res = classifier(request.text)
+    return SentimentAnalysisResponse(analysis = "TEST")
+
 
 def start():
     """Launched with `poetry run start` at root level"""
