34ec599cce13d4d304aa0a82e0ba5ff2473fe347
Matt Moore <matt@mattmoore.io>
2025-02-27 00:07:44 -0500

    Custom interceptor example

--- src/axon/routes.py
+++ src/axon/routes.py
@@ -1,15 +1,20 @@
 import uvicorn
 from fastapi import FastAPI
 from pydantic import BaseModel
-from prometheus_client import Counter
+from typing import Callable
 from prometheus_fastapi_instrumentator import Instrumentator
+from prometheus_fastapi_instrumentator.metrics import Info
+from prometheus_client import Counter
 
 from .ai.sentiment_analysis import sentiment_analysis
 from .ai.ask import ask
 
 app = FastAPI()
 
-instrumentator = Instrumentator().instrument(app, metric_namespace = 'axon')
+instrumentator = Instrumentator(
+    excluded_handlers = [".*admin.*", "/metrics"],
+)
+instrumentator.instrument(app, metric_namespace = 'axon', metric_subsystem = 'ai')
 instrumentator.expose(app, include_in_schema = False, should_gzip = True)
 
 @app.get("/")
@@ -38,6 +43,27 @@ class AskRequest(BaseModel):
 
 class AskResponse(BaseModel):
     answer: str
+
+
+# Note: Can set up custom interceptors with this
+#def http_requested_languages_total() -> Callable[[Info], None]:
+#    METRIC = Counter(
+#        "http_requested_languages_total",
+#        "Number of times ask API is queried.",
+#        labelnames = ("lang",)
+#    )
+#
+#    def instrumentation(info: Info) -> None:
+#        langs = set()
+#        lang_str = info.request.headers["Accept-Language"]
+#        for element in lang_str.split(","):
+#            element = element.split(";")[0].strip().lower()
+#            langs.add(element)
+#        for language in langs:
+#            METRIC.labels(language).inc()
+#
+#    return instrumentation
+#instrumentator.add(http_requested_languages_total())
 
 ask_counter = Counter('custom_ask_counter', 'Ask query counter')
 @app.post("/api/ask")
